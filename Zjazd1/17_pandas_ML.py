f111e677c23923f84e8ce52b739eb698ed3789ed
student-wroclaw <student-wroclaw@EDU-WSB.local>
2024-11-24 14:44:58 +0100

    1

--- Zjazd1/17_pandas_ML.py
+++ Zjazd1/17_pandas_ML.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+from sklearn.linear_model import LinearRegression
+
 
 df = pd.read_csv('weight-height.csv')
 # print(type(df))
@@ -17,6 +19,22 @@ print(df.head(3))
 # sns.histplot(df.Weight)
 # plt.show()
 
-sns.histplot(df.query("Gender=='Male'").Weight)
-sns.histplot(df.query("Gender=='Female'").Weight)
-plt.show()
+# sns.histplot(df.query("Gender=='Male'").Weight)
+# sns.histplot(df.query("Gender=='Female'").Weight)
+# plt.show()
+
+print(df.describe())
+
+df = pd.get_dummies(df)  #zmiana na dane numeryczne
+print(df.head(3))
+del df['Gender_Male']
+print(df.head(3))
+df.rename(columns={'Gender_Female': "Gender"}, inplace=True)
+print(df.head(3))
+# 0 - facet, 1 - kobieta
+
+model = LinearRegression()
+model.fit(df[['Height', 'Gender']],  df['Weight'] )
+print(f'Wspolczynnik kierunkowy: {model.coef_}')
+print(f'Wyraz wolny: {model.intercept_}')
+print('wzór: Height * ',model.coef_[0], '+ Gender * ',model.coef_[1], '+ ',model.intercept_)
