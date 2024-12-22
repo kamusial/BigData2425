b20e71e94e5b697405b1e2c7452d07db7bfd14c2
belfer wroclaw <belfer-wroclaw@EDU-WSB.local>
2024-12-22 10:34:31 +0100

    '1'

--- Zjazd2/4_logowanie_rejestracja.py
+++ Zjazd2/4_logowanie_rejestracja.py
@@ -1,20 +1,27 @@
 # import functions
 from functions import *
 
-login = input('Podaj login:   ')
-passwd = input('Podaj hasło:   ')
+while True:
+    decission = input('Logowanie bądź Rejestracja  L / R')
+    if decission[0].lower() == 'l':
+        login = input('Podaj login:   ')
+        passwd = input('Podaj hasło:   ')
+        if user_exist(login) and password_correct(login, passwd):
+            print('użytkownik zalogowany')
+            break
+        else:
+            print('Login bądź hasło niepoprawne')
+    elif decission[0].lower() == 'r':
+        login = input('Podaj login:   ')
+        if not user_exist(login):
+            passwd1 = input('Podaj haslo: ')
+            passwd2 = input('Potwierdź haslo: ')
+            if passwd1 == passwd2:
+                if passwd_valid(passwd1):
+                    add_user(login, passwd1)
+                    break
+    else:
+        print('Niepopranwy wybór')
 
-if user_exist(login) and password_correct(login, passwd):
-    print('użytkownik zalogowany')
 
-
-
-
-else:
-    print(f'Rejestracja {login}')
-    users[login] = passwd
-
-
-special_caracters = {',.;/\\\'[]'}
-len(set(passwd) & special_caracters)
 
--- Zjazd2/functions.py
+++ Zjazd2/functions.py
@@ -1,4 +1,6 @@
 users = {'Kamil': '123', 'Mario': 'M11'}
+characters = {',./;\'[]'}
+digits = {'0123456789'}
 
 
 def user_exist(login):
@@ -11,3 +13,28 @@ def password_correct(login, passwd):
     if users[login] == passwd:
         return True
     return False
+
+def passwd_valid(passwd):
+    counter = 0
+    if passwd != passwd.upper():
+        print('Znaleziono małą literę')
+        counter += 1
+    if passwd != passwd.lower():
+        print('Znaleziono dużą literę')
+        counter += 1
+    if len(set(passwd) & characters) > 0:
+        print('Znaleziono znak specjalny')
+        counter += 1
+    if len(set(passwd) & digits) > 0:
+        print('Znaleziono cyfrę')
+        counter += 1
+    if counter == 4:
+        return True
+    return False
+
+def add_user(login, passwd):
+    users[login] = passwd
+    print('Użytkownik dodany')
+
+
+
