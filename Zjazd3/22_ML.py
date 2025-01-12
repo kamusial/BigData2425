511ef17e8a5332e41de21494e87d703c45ea405b
belfer wroclaw <belfer-wroclaw@EDU-WSB.local>
2025-01-12 14:44:15 +0100

    1

--- Zjazd3/22_ML.py
+++ Zjazd3/22_ML.py
@@ -8,16 +8,10 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import confusion_matrix
 
-X, y = make_circles(n_samples=100, factor=0.6, noise=0.2)
-
-print('\n\nX"y\n', X)
-print('\n\nY"i\n', y)
-
-plt.scatter(X[:, 0], X[:, 1], c=y)
-plt.show()
-
+df = pd.read_csv('data\\heart.csv', comment='#')
+X = df.iloc[:, :-1]
+y = df.target
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.2)
-
 
 print('\nLogistic Regression')
 model = LogisticRegression()
@@ -26,13 +20,13 @@ print(model.score(X_test, y_test))
 print(pd.DataFrame(confusion_matrix(y_test, model.predict(X_test))))
 
 print('\nKNN')
-model = KNeighborsClassifier() #n_neighbors=500, weights='distance')
+model = KNeighborsClassifier(n_neighbors=12, weights='distance')
 model.fit(X_train, y_train)
 print(model.score(X_test, y_test))
 print(pd.DataFrame(confusion_matrix(y_test, model.predict(X_test))))
 
 print('\nDecission Tree')
-model = DecisionTreeClassifier(max_depth=3, min_samples_split=3)
+model = DecisionTreeClassifier(max_depth=3, min_samples_split=5)
 model.fit(X_train, y_train)
 print(model.score(X_test, y_test))
 print(pd.DataFrame(confusion_matrix(y_test, model.predict(X_test))))
@@ -42,4 +36,3 @@ model = SVC()
 model.fit(X_train, y_train)
 print(model.score(X_test, y_test))
 print(pd.DataFrame(confusion_matrix(y_test, model.predict(X_test))))
-
