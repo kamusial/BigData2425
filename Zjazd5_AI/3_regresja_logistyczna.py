40bfeec495344faa32b59f5add97a96df61c0985
belfer wroclaw <belfer-wroclaw@EDU-WSB.local>
2025-02-08 14:53:55 +0100

    '1'

--- Zjazd5_AI/3_regresja_logistyczna.py
+++ Zjazd5_AI/3_regresja_logistyczna.py
@@ -18,4 +18,30 @@ for col in ['glucose', 'bloodpressure', 'skinthickness', 'insulin',
 print('Po czyszczeniu')
 print(df.describe().T.to_string())
 
+from sklearn.linear_model import LogisticRegression   #regresja jest tu
+from sklearn.model_selection import train_test_split   #klasa jest tu
+from sklearn.metrics import confusion_matrix
+
 df.to_csv('dane\\cukrzyca.csv', index=False)
+
+X = df.iloc[: , :-1]
+y = df.outcome
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+model = LogisticRegression()
+model.fit(X_train, y_train)
+print(model.score(X_test, y_test))
+print(pd.DataFrame( confusion_matrix(y_test, model.predict(X_test) ) ))
+
+print('Zmiana danych')
+print(df.outcome.value_counts())
+df1 = df.query("outcome==0").sample(n=500)
+df2 = df.query("outcome==1").sample(n=500)
+df3 = pd.concat([df1, df2])
+
+X = df3.iloc[: , :-1]
+y = df3.outcome
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+model = LogisticRegression()
+model.fit(X_train, y_train)
+print(model.score(X_test, y_test))
+print(pd.DataFrame( confusion_matrix(y_test, model.predict(X_test) ) ))
---
+++ Zjazd5_AI/4_KNN.py
@@ -0,0 +1,43 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix
+
+df = pd.read_csv('dane\\iris.csv')
+print(df['class'].value_counts())
+
+species = {
+    'Iris-setosa': 0, 'Iris-versicolor': 1, 'Iris-virginica': 2
+}
+df['class_value'] = df['class'].map(species)
+print(df)
+sample = [5.6, 3.2, 5.2, 1.45]
+
+sns.scatterplot(data=df, x='sepallength', y='sepalwidth', hue='class')
+plt.scatter(5.6, 3.2, c='r')
+plt.show()
+
+sns.scatterplot(data=df, x='petallength', y='petalwidth', hue='class')
+plt.scatter(5.2, 1.45, c='r')
+plt.show()
+
+print('Klasyfikator')
+X = df.iloc[:, 2:4]
+y = df.class_value
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+model = KNeighborsClassifier(n_neighbors=100, weights='distance')  #distance, uniform
+model.fit(X_train, y_train)
+print(model.score(X_test, y_test))
+print(pd.DataFrame(confusion_matrix(y_test, model.predict(X_test))))
+
+results = []
+for k in range(1, 101):
+    model = KNeighborsClassifier(n_neighbors=k, weights='distance')
+    model.fit(X_train, y_train)
+    results.append(model.score(X_test, y_test))
+
+plt.plot(range(1, 101), results)
+plt.show()
