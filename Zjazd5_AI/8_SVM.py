cc8a0614691f466aa26926c856f2bbffbc84be6b
student-wroclaw <student-wroclaw@EDU-WSB.local>
2025-02-09 10:42:42 +0100

    'a'

--- Zjazd5_AI/8_SVM.py
+++ Zjazd5_AI/8_SVM.py
@@ -7,3 +7,16 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import confusion_matrix
+
+df = pd.read_csv('dane\\heart.csv', comment='#')
+print(df.head().to_string())
+
+X = df.iloc[:, :-1]
+y = df.target
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
+model = SVC(verbose=True)
+model.fit(X_train, y_train)
+print(model.score(X_test, y_test))
+print(pd.DataFrame(confusion_matrix(y_test, model.predict(X_test))))
+
