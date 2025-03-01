cf511ffc5c413cd5e60135ee97b3b30f0b86bfa8
student-wroclaw <student-wroclaw@EDU-WSB.local>
2025-03-01 09:47:01 +0100

    1

--- Zjazd5_AI/15_circle.py
+++ Zjazd5_AI/15_circle.py
@@ -2,8 +2,18 @@ import matplotlib.pyplot as plt
 from sklearn.datasets import make_circles
 import pandas as pd
 
-X, y = make_circles(n_samples=10, factor=0.8, noise=0)
+X, y = make_circles(n_samples=100, factor=0.6, noise=0.1)
 print(X)
 print(y)
-plt.scatter(X[:, 0], X[:, 1]     )
+plt.scatter(X[:, 0], X[:, 1]   , c=y  )
 plt.show()
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.2, random_state=0)
+
+
+from sklearn.linear_model import LogisticRegression
+print('Regresja logistyczna')
+model = LogisticRegression()
+model.fit(X_train, y_train)
+print(model.score(X_test, y_test))
